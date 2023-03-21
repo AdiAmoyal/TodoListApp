450637baec03e8b25c79bacc051c0733a88292e2
Adi Amoyal <adi.amoyal02@gmail.com>
2023-03-21 17:19:54 +0200

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Adi Amoyal on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
